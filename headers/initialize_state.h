080c5769d6d6984779b047b5a0defa2307020636
Mat <109213225+MatSu99@users.noreply.github.com>
2023-06-10 00:07:49 +0200

    [DEV] Added StateID enum.
    New enum has been added to represent states ids.

--- headers/initialize_state.h
+++ headers/initialize_state.h
@@ -10,5 +10,18 @@ class InitializeState: StateBase {
     InitializeState(int ID);
     ~InitializeState();
 
+    enum StateID
+    {
+        INITIALIZE_STATE = 1,
+        MAINTENANCE_STATE = 2,
+        CHECK_RESOURCES_STATE = 3,
+        START_STATE = 4,
+        WAIT_FOR_INPUT_STATE = 5,
+        WAIT_FOR_DECISION_PAYMENT_STATE = 6,
+        CASH_STATE = 7,
+        CARD_STATE = 8,
+        ISSUE_PRODUCT = 9
+    };
+
     private:
 };
